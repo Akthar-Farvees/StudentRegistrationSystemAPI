36f10b8fa6e001278a4380213785442bf9cdbccd
Akthar farvees <junaideenakthar123@gmail.com>
2023-08-14 10:58:29 +0530

    Add ASP.NET Core Web API project to StuReg-Web-API branch

---
+++ StudentRegistrationSystemAPI/Controllers/StudentRegistrationController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StudentRegistrationSystemAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentRegistrationController : ControllerBase
+    {
+        [HttpGet]
+
+        public async Task<ActionResult<List<StudentRegistration>>> GetStudentRegistration()
+        {
+            return new List<StudentRegistration>
+            {
+                new StudentRegistration
+                {
+                    FirstName = "Akthar",
+                    LastName = "Farvee",
+                    Mobile = "+94757649401",
+                    Email = "akthar123@gmail.com",
+                    DOB = "1998-12-10",
+                    Address = "Colombo, Marudhana"
+
+                }
+            };
+        }
+    }
+}
---
+++ StudentRegistrationSystemAPI/StudentRegistration.cs
@@ -0,0 +1,14 @@
+﻿namespace StudentRegistrationSystemAPI
+{
+    public class StudentRegistration
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Mobile { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string DOB { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+
+    }
+}
